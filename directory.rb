6c0aae2ee5569909c1c755e22e3bbbcd2003425c
Michael Su <mailmsuu@gmail.com>
2022-03-15 11:37:45 +0000

    Interactive menu

--- directory.rb
+++ directory.rb
@@ -1,8 +1,8 @@
+@students = []
+
 def input_students
   puts "Enter the names of the students"
   puts "When finished, just hit return twice"
-  # Create an empty array
-  students = []
   # Get the first student's name
   name = gets.chomp
   # A loop for while the name is not empty
@@ -24,23 +24,22 @@ def input_students
       if cohort =~ /[A-Z]/
         cohort = cohort.downcase!
       end
-      if cohort == "november" || cohort == "april"
+      cohort = cohort.capitalize!
+      if cohort == "November" || cohort == "April"
         break
       end
     end
     # Add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym, hobby: hobby, country: country, height: height}
-    if students.count == 1
+    @students << {name: name, cohort: cohort.to_sym, hobby: hobby, country: country, height: height}
+    if @students.count == 1
       puts "Now we have 1 student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     # Get the name of the next student
     puts "What is the name of the next student?"
     name = gets.chomp
   end
-  # Return the array of students
-  students
 end
 
 def print_header
@@ -48,62 +47,67 @@ def print_header
   puts "------------".center(25)
 end
 
-def print(students)
-  students.each_with_index do |student|
+def print_students_list
+  @students.each_with_index do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)".center(25)
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(25)
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(25)
 end
 
-def print_with_index(students)
-  students.each_with_index do |student, index| 
+def print_with_index
+  @students.each_with_index do |student, index| 
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
   
 
-def name_begin_with(students, letter)
-  beginwithletter = students.select do |student|
+def name_begin_with(letter)
+  beginwithletter = @students.select do |student|
     student[:name].downcase.start_with?(letter.downcase) 
   end
   puts "Students with names beginning with the letter '#{letter}'"
   beginwithletter.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]})"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def name_shorter_than(students, number)
-  shorterthan = students.select do |student|
+def name_shorter_than(number)
+  shorterthan = @students.select do |student|
     student[:name].length < number 
   end
   puts "Students with names with less than #{number} characters"
   shorterthan.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]})"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def new_print(students)
+def new_print
 index = 0
-  while index < students.count do
-    puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
+  while index < @students.count do
+    puts "#{index + 1}. #{@students[index][:name]} (#{@students[index][:cohort]} cohort)"
   index += 1
   end
 end
 
-def november_cohort(students)
-  students.each do |student|
-    if student[:cohort] == :november
+def single_cohort(cohort)
+  if cohort =~ /[A-Z]/
+    cohort = cohort.downcase!
+  end
+  cohort = cohort.capitalize!
+  puts "Students in the #{cohort} cohort"
+  @students.each do |student|
+    if student[:cohort] == cohort.to_sym
       puts student[:name]
     end
   end
 end
 
-def grouped_by_cohort(students)
+def grouped_by_cohort
   cohortgroup = {}
-  students.each do |student|
+  @students.each do |student|
     if cohortgroup[student[:cohort]] == nil
       cohortgroup[student[:cohort]] = []
     end
@@ -111,30 +115,52 @@ def grouped_by_cohort(students)
   end
 
   cohortgroup.each do |cohort, students|
-    puts "Students in the #{cohort.capitalize} cohort"
+    puts "Students in the #{cohort} cohort"
     students.each do |student| puts student end
   end
 end
 
-def print_if_not_empty(students)
-  if students.count > 0
+def print_if_not_empty
+  if @students.count > 0
     puts "The students of Villains Academy".center(25)
     puts "------------".center(25)
-    students.each_with_index do |student|
+    @students.each_with_index do |student|
       puts "#{student[:name]} (#{student[:cohort]} cohort)".center(25)
     end
-    puts "Overall, we have #{students.count} great students".center(25)
+    puts "Overall, we have #{@students.count} great students".center(25)
   end
 end
 
-students = input_students
-print_header
-print_with_index(students)
-print_footer(students)
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
 
-name_begin_with(students, "a")
-name_shorter_than(students, 12)
-new_print(students)
-november_cohort(students)
-grouped_by_cohort(students)
-print_if_not_empty(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit 
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+interactive_menu
